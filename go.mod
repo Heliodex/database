a65c122a1a782f7e6b811200384b270d7de5045f
Lewin Kelly <heli@odex.cf>
2023-08-09 06:15:38 +0100

    Improve logo printing in command line with colours

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Heliodex/database
 
 go 1.20
+
+require github.com/TwiN/go-color v1.4.0
--- main.go
+++ main.go
@@ -4,23 +4,24 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	c "github.com/TwiN/go-color"
 )
 
 const v = "0.0.0"
 
 func main() {
-	fmt.Println(`
-      ╭─▅▅                             ╭─▅▅
-      │ ██             ╭─██            │ ██
- ╭─▅███▅██ ╭─▅████▄█▌╭─██████╭─▅████▄█▌│ ██▅███▄  ╭─▅████▄█▌╭▗▇█🮅🮅▆▖ ╭─▅█🮅🮅█▅
-╭╯██▘  ▝██╭╯▐█▛   ▜█▌╰─╮ ██─┼╯▐█▛   ▜█▌│ ██▘  ▝██╭╯▐█▛   ▜█▌│▝█▙▄▄▃╯╭╯██▄▄▄▄█▋
-│ ██▖  ▗██│ ▐█▙   ▟█▌  │ ██ │ ▐█▙   ▟█▌│ ██▖  ▗██│ ▐█▙   ▟█▌╰╮ 🮃▀▜█▋│ ██🮂🮂🮂🮂🮂
-╰╮ ▀███▀██╰╮ 🮄████▀█▌  │ ██ ╰╮ 🮄████▀█▌│ ██▀███▀╯╰╮ 🮄████▀█▌╭▝🮅▆▆█🮅╯╰╮ 🮄█▆▆▆🮅
- ╰───╨──╯  ╰─────╨─╯   ╰──╯  ╰─────╨─╯ ╰──╨───╯   ╰─────╨─╯ ╰─────╯  ╰──────╯
-	`)
+	// This says "database" in unicode art I promise
+	fmt.Print(c.InRed("      ╭─▅▅"),  "                             ", c.InPurple("╭─▅▅")+"\n")
+	fmt.Print(c.InRed("      │ ██"),  "           ", c.InGreen("  ╭─██  "),  "          ", c.InPurple("│ ██ Version "), c.InRed(v)+"\n")
+	fmt.Print(c.InRed(" ╭─▅███▅██"), c.InYellow(" ╭─▅████▄█▌"), c.InGreen("╭─██████"), c.InBlue("╭─▅████▄█▌"), c.InPurple("│ ██▅███▄ "), c.InRed(" ╭─▅████▄█▌"), c.InYellow("╭▗▇█▀🮅▆▖"), c.InGreen("╭─▅█🮅🮅█▅")+"\n")
+	fmt.Print(c.InRed("╭╯██▘─╮▝██"), c.InYellow("╭╯▐█▛─╮ ▜█▌"), c.InGreen("╰─╮ ██─┼"), c.InBlue("╯▐█▛─╮ ▜█▌"), c.InPurple("│ ██▘─╮▝██"), c.InRed("╭╯▐█▛─╮ ▜█▌"), c.InYellow("│▝█▙▄▃╮"), c.InGreen(" ╭╯██▄▄▄▄█▋")+"\n")
+	fmt.Print(c.InRed("│ ██▖ │▗██"), c.InYellow("│ ▐█▙ │ ▟█▌"), c.InGreen("  │ ██ "), c.InBlue("│ ▐█▙ │ ▟█▌"), c.InPurple("│ ██▖ │▗██"), c.InRed("│ ▐█▙ │ ▟█▌"), c.InYellow("╰╮ ▀▀▜█▖"), c.InGreen("│ ██🮂🮂🮂🮂🮂")+"\n")
+	fmt.Print(c.InRed("╰╮ ▀███▀██"), c.InYellow("╰╮ ▀████▀█▌"), c.InGreen("  │ ██ "), c.InBlue("╰╮ ▀████▀█▌"), c.InPurple("│ ██▀███▀╯"), c.InRed("╰╮ ▀████▀█▌")+ c.InYellow("╭▝🮅▆▆█▀"), c.InGreen("╰╮ 🮄█▆▆▆🮅")+"\n")
+	fmt.Print(c.InRed(" ╰───╨──╯ "), c.InYellow(" ╰─────╨─╯ "), c.InGreen("  ╰──╯  "), c.InBlue("╰─────╨─╯"), c.InPurple(" ╰──╨───╯  "), c.InRed(" ╰─────╨─╯ "), c.InYellow("╰─────╯"), c.InGreen("  ╰──────╯")+"\n")
 
 	if len(os.Args) < 2 {
-		fmt.Println("Please specify a command")
+		fmt.Println(c.InRed("No command specified. Run 'database help' for more information."))
 		os.Exit(1)
 	}
 
@@ -28,7 +29,11 @@ func main() {
 	case "version", "v", "--version", "-v":
 		fmt.Println("Version", v)
 	case "help", "h", "--help", "-h":
-		fmt.Println("Help")
-		help()
+		help(v)
+	case "start", "s", "--start", "-s":
+		start()
+	default:
+		fmt.Println(c.InRed("Unknown command. Run 'database help' for more information."))
+		os.Exit(1)
 	}
 }
